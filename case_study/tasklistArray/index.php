caff6c4aa27da690e203eacf1794bbf483f3023a
fiestazzz <Salvatore.Botta@outlook.com>
2021-03-08 10:24:33 +0100

    Versione HTML del layout

--- case_study/tasklistArray/index.php
+++ case_study/tasklistArray/index.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./asset/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+    <input type="text" name="searchText">
+    <button type="submit">Cerca</button>
+    </form>
+    <ul>
+    <li class="tasklist-item tasklist-item-progress">Latte <b>progress</b> </li>
+    <li class="tasklist-item tasklist-item-done">Uova - <b>done</b> </li>
+    <li class="tasklist-item tasklist-item-todo">Uova - <b>todo</b></li>
+    </ul>
+</body>
+</html>
